1cb157753d14904359d6135518d2d0ceeb5a8e36
Kienroro2003 <nguyentrankien281@gmail.com>
2025-09-15 19:45:48 +0700

    change port to 4000

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     # Chạy ứng dụng ở chế độ debug
     # host='0.0.0.0' để có thể truy cập từ bên ngoài container (nếu bạn chạy app trong Docker)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=4000, debug=True)
