76311b7dcdc30312aaad5845179675557558a08c
Kienroro2003 <nguyentrankien281@gmail.com>
2025-09-15 19:58:17 +0700

    config logger for __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
+import logging
+
 # app/__init__.py
 import os
 import sys
+from logging.handlers import RotatingFileHandler
 
 from flask import Flask
 
@@ -15,6 +18,32 @@ def create_app():
 
     # Tải cấu hình từ class Config
     app.config.from_object(Config)
+    if not app.debug:
+        # Tạo thư mục logs nếu chưa tồn tại
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+
+        # Cấu hình ghi log ra file
+        file_handler = RotatingFileHandler(
+            "logs/note_system.log", maxBytes=10240, backupCount=10
+        )
+
+        # Cấu hình định dạng của log message
+        file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+            )
+        )
+
+        # Thiết lập cấp độ log
+        file_handler.setLevel(logging.INFO)
+
+        # Gắn handler vào logger của app
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Note System startup")
+    # -----------------------
 
     # Đăng ký blueprint (chứa các routes API)
     from .routes import api as api_blueprint
